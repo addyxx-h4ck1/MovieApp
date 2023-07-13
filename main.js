436bdafd42b0db84c946488b7211cbc7ca39d152
addyxx-h4ck1 <119793506+addyxx-h4ck1@users.noreply.github.com>
2023-07-13 13:13:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,110 +1,57 @@
-/*  ROCK, PAPER SCISSORS GAME ENGINE JS */
+const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=97e64ceadf5d68cc5bb441a54b2b62b0&page=1";
+const IMG_PATH ="https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=97e64ceadf5d68cc5bb441a54b2b62b0&query=";
 
+const main = document.getElementById ("section")
+const form = document.getElementById ("form")
+const search = document.getElementById ("query")
 
-//player DOM selectors
-const playerOneChoice = document.querySelectorAll(".choose")
-//game function 
- const gamePlay = function(){
-    //  navigate DOM player choices - {a for loop}
-    /* FOR LOOP  */
-    for (let i = 0; i < playerOneChoice.length; i++){
-        let plresult = playerOneChoice[i].textContent;
-            playerOneChoice[i].addEventListener('click', (e)=>{
-            document.querySelector(".plchoice").textContent = plresult;
+returnMovies(APILINK)
+function returnMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+        console.log(data.results);
+        data.results.forEach(element => {
+            const div_card = document.createElement('div');
+            div_card.setAttribute("class","card");
 
-    // Randomize computer choice using math random function && displaying choices to DOM
-    // computer choices {array}
-    const computerChoice = ["rock", "paper", "scissors"];
-        let computer = computerChoice[Math.floor(Math.random () * computerChoice.length)];
-        console.log(computer);
+            const div_row = document.createElement('div')
+            div_card.setAttribute("class","row");
 
-        document.querySelector(".quit").textContent = "Reset Score"
-        //sending comp. choice to DOM
-        document.querySelector(".cpchoice").textContent = computer;
+            const div_column = document.createElement('div')
+             div_column.setAttribute("class","column");
 
-        // declaring and sending player choice to DOM
-        let playerone = document.querySelector(".plchoice").textContent;
+            const image = document.createElement('img')
+            image.setAttribute("class","thumbnail");
+            image.setAttribute("id","image");
 
-// decision tree
-    let resultBe = computer === playerone ? "Tie Game": computer === "rock" && playerone === "scissors" ?  "computer wins": computer === "paper" && playerone === "rock" ?  "computer wins" : computer === "scissors" && playerone === "paper" ?  "computer wins" 
-    : "player One wins"
-    
-    //Sending result to DOM
-    document.querySelector(".outcome").textContent = resultBe;
-    
- // score variables 
-    let computerScore = 0;
-    let playeroneScore = 0;
-    let tieGame = 0;
+            const title = document.createElement('h3')
+            title.setAttribute("id","title");
 
-    // Determining winner - (if statements)
-    if(resultBe === "Tie Game"){
-        playeroneScore+=.5;
-        computerScore+=.5;
-        tieGame++;
-    }else if(resultBe === "computer wins"){
-        computerScore++;
-    }else if(resultBe === "player One wins"){
-        playeroneScore++;
-    }
-    // setting computer score storage
-    const getComputerStore = localStorage.getItem("computerScore")
-    const total = Number(getComputerStore) + Number(computerScore);
-    const  computerStore = localStorage.setItem("computerScore", total);
-    
-    //playerOne score storage
-    const getplayerStore = localStorage.getItem("playerScore")
-    const playerTotal = Number(getplayerStore) + Number(playeroneScore);
-    const  playerStore = localStorage.setItem("playerScore", playerTotal);
+            const center = document.createElement('center')
+            
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
 
-    //playerOne score storage
-    const gettieStore = localStorage.getItem("tieStore")
-    const tieTotal = Number(gettieStore) + Number(tieGame);
-    const  tieStore = localStorage.setItem("tieStore", tieTotal);
-    //Displaying scores to the  DOM
-    document.querySelector(".computer-score").textContent = localStorage.getItem("computerScore");
-    document.querySelector(".player0ne-score").textContent = localStorage.getItem("playerScore");
-    document.querySelector(".tie-score").textContent = localStorage.getItem("tieStore") + " Time(s)";
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            main.appendChild(div_row);
 
-///////////////////ADDITIONS///////
-        if(localStorage.getItem("computerScore") > localStorage.getItem("playerScore")) {
-            document.querySelector(".leader").textContent = "Computer leads"
-        }else if(localStorage.getItem("computerScore") < localStorage.getItem("playerScore")){
-            document.querySelector(".leader").textContent = "Player One leads"
-        }
-        else if(localStorage.getItem("computerScore") === localStorage.getItem("playerScore")){
-            document.querySelector(".leader").textContent = "";
-        }
-
-        //// times game played
-        let totalPlayed = Number(localStorage.getItem("computerScore")) + Number(localStorage.getItem("playerScore"));
-
-        document.querySelector(".played").textContent = ` ${totalPlayed} Time(s)`;
-
-
-     /*  END OF GAME FUNC... */
-})
-    /* END OF FOR LOOP */
+        });
+    });
 }
-};
- // Calling game function
 
-gamePlay();
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    main.innerHTML = ""
 
-//reset score
-const resetScore = document.querySelector(".quit");
-resetScore.addEventListener("click", (e)=>{
-   localStorage.clear();
-   resetScore.textContent = "done";
-    document.querySelector(".computer-score").textContent = "0"
-    document.querySelector(".player0ne-score").textContent = "0"
-    document.querySelector(".tie-score").textContent =  "0";
-    document.querySelector(".cpchoice").textContent = "";
-    document.querySelector(".plchoice").textContent = "";
-    document.querySelector(".outcome").textContent = "Rock Paper or Scissors?";
-    document.querySelector(".leader").textContent = "";
-    document.querySelector(".played").textContent = "";
-});
+    const searchItem = search.value;
 
-/* END OF CODE... */
-
+    if(searchItem){
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = ""
+    }
+})
